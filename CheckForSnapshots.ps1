a6ad0892bf2ad6efc0c1bc621867c83266970468
tantonovich <tantonovich@resdat.com>
2014-07-08 15:39:47 -0800

    Update CheckForSnapshots.ps1

--- CheckForSnapshots.ps1
+++ CheckForSnapshots.ps1
@@ -4,10 +4,10 @@ Add-PSSnapin VMWare.VimAutomation.Vds
 
 $vmCredential = Get-Credential
 
-Connect-VIserver hqappvc1 -Credential $vmCredential
+Connect-VIserver vc1 -Credential $vmCredential
 $vm = Get-VM
 Get-Snapshot $vm | ft VM,Created,PowerState
 
-Connect-VIserver aficappvc1 -Credential $vmCredential
+Connect-VIserver vc2 -Credential $vmCredential
 $vm = Get-VM
 Get-Snapshot $vm | ft VM,Created,PowerState
